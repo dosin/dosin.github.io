a2957e10a9b0a37300f1cbacb303f4c9a7654809
dosin <420638791@qq.com>
2016-08-10 13:10:46 +0800

    update index.html/css/js

--- scripts/common.js
+++ scripts/common.js
@@ -1,21 +1,23 @@
-
 /* selector */
 
-var get = function(selector){return document.querySelector(selector);};
+var get = function(selector) {
+	return document.querySelector(selector);
+};
 
 /* set target */
 
-(function(){
-	var blank=document.getElementsByTagName("a");
-	for(var i = blank.length - 1; i >= 0; i--){
+(function() {
+	var blank = document.getElementsByTagName("a");
+	for (var i = blank.length - 1; i >= 0; i--) {
 		blank[i].target = "_blank";
 	}
 	self(document.getElementsByTagName('nav')[0]);
 	self(document.getElementById('blog'));
+
 	function self(parent) {
 		if (parent) {
 			var eles = parent.getElementsByTagName('a');
-			for(var i = eles.length - 1; i >= 0; i--){
+			for (var i = eles.length - 1; i >= 0; i--) {
 				eles[i].target = "_self";
 			}
 		}
@@ -24,15 +26,15 @@ var get = function(selector){return document.querySelector(selector);};
 
 /* nav-menu */
 
-$(function(){
-	var i=location.pathname=="/"||location.pathname=="/website/"?1:
-		location.pathname=="/articles/"||location.pathname=="/website/articles/"?2:
-		location.pathname=="/more/"||location.pathname=="/website/more/"?3:
-		location.pathname=="/about/"||location.pathname=="/website/about/"?4:
+$(function() {
+	var i = location.pathname == "/" || location.pathname == "/website/" ? 1 :
+		location.pathname == "/articles/" || location.pathname == "/website/articles/" ? 2 :
+		location.pathname == "/more/" || location.pathname == "/website/more/" ? 3 :
+		location.pathname == "/about/" || location.pathname == "/website/about/" ? 4 :
 		null;
 	$(".menu").prepend("<ul><li></li><li></li><li></li></ul>")
 	$(".quickdelete").prepend("<ul><li></li><li></li></ul>")
-	$(".nav-menu").on("click",function(){
+	$(".nav-menu").on("click", function() {
 		$(this).next().toggle()
 	})
 })
